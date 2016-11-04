91ea121cedf0b8f67132a32a121134a767790bad
Luciano Uribe <lucianouribe@gmail.com>
2016-11-03 19:47:48 -0600

    Asigning loop first version

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+ruby '2.3.1'
+
+# Use binding.pry to debug your code
+gem 'pry', '~> 0.10.4'
---
+++ asignador.rb
@@ -0,0 +1,37 @@
+require 'pry'
+class Alfabeto
+attr_accessor :latino, :cirilico
+
+  def initialize(latino, cirilico)
+    @latino = latino
+    @cirilico = cirilico
+  end
+
+end
+
+
+class Translation
+
+attr_accessor :alfabeto
+
+  def initialize
+    @latino = %w(ñ ch ll rr qu a b ce ci c d e f ge gi gu g h i j k l m n o p q r s t u v w x y y zz z)
+    @cirilico = ['н', 'ч', 'ж', 'р', 'к', 'a', 'б', 'сэ', 'си', 'к', 'д', 'э', 'ф', 'хэ', 'хи', 'г', 'г', "\s", 'и', 'х', 'к', 'л', 'м', 'н', 'о', 'п', 'к', 'р', 'с', 'т', 'у', 'в', 'у', 'ц', 'и', 'ж', 'ц', 'з']
+    @alfa = []
+    translate
+    printer
+  end
+
+  def translate
+    @latino.each_with_index do |x, i|
+        @alfa << Alfabeto.new(@latino[i], @cirilico[i])
+    end
+  end
+
+  def printer
+    puts "#{@alfa[1].cirilico}"
+    puts "#{@alfa[1].latino}"
+  end
+end
+
+Translation.new
